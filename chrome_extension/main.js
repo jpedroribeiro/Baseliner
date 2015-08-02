6732ace7b2558c37e86994f779f74ed1965fabcc
J. Pedro Ribeiro <jpedroribeiro@joaopar-air.local>
2015-08-02 16:55:37 +0100

    Tidy up

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,14 +1,14 @@
-// Runs Baseliner script on tab
+/* Runs Baseliner script on tab */
 chrome.tabs.executeScript(null, {file: "baseliner_v1.0.0.js"});
 
 
-// Sets variables
+/* Sets variables */
 var extensionTop        = document.getElementById('baselinerTop');
 var extensionBaseline   = document.getElementById('baselinerValue');
 var extensionBtnRemove  = document.getElementById('btnRemoveBaseliner');
 
 
-// Executable function
+/* Executes || Updates Baseliner */
 var updatesBaseliner = function(){
     var newTop  = extensionTop.value || 0;
     var newBase = extensionBaseline.value || 0;
@@ -19,36 +19,31 @@ var updatesBaseliner = function(){
 };
 
 
+/* Executes removeBaseliner */
 var removeBaseliner = function(){
     chrome.tabs.executeScript({
         code: 'Baseliner.removeBaseliner()'
     });
 };
 
-// Set event listeners
+
+/* Set event listeners */
 extensionBaseline.addEventListener('input', updatesBaseliner);
 extensionTop.addEventListener('input', updatesBaseliner);
 extensionBtnRemove.addEventListener('click', removeBaseliner);
 
 
-
-
-
-
-
-
-
-
 /*
 TODO: Chrome Extension
+
 DONE) Auto remove on Init
-2) add 'Remove' button
-3) -
-3a) Integrate executable functions into one "run()"?
+DONE) add 'Remove' button
+NOPE) Integrate executable functions into one "run()"?
 4) create icon for chrome extension
 5) see other options for manifest.json
 6) style chrome extension popup
 7) comment/document everything
 0) Review DEMO and see if it can use same as chrome version of JS
 8) offer minified version on demo
+
  */
