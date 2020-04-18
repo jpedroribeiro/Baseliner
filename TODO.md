97004faf803910223d634f80492de2198b984796
J. Pedro Ribeiro <joaopar@gmail.com>
2020-04-18 16:23:18 +0100

    Moving version 6 to repo

---
+++ TODO.md
@@ -0,0 +1,14 @@
+WIP
+
+# TODO
+(1) Nicer UI design
+(1.5) Move defaults to object
+(2) Add Typescript
+(3) GA events?
+(4) Domain and website?
+
+# Notes
+- Double check if manifest's permission are OK due to chunk hashes being dynamic
+
+# Future ideas
+- Use chrome.tabs.insertCSS instead of creating style tag: the caveat is that removeCSS is not supported in Chrome yet (FF only)
---
+++ build/precache-manifest.ddf8dea76a17261e6a6d70736f55d862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfccc804d2052e7dd38a28f845415e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "503d4ebce8d05e16e016",
+    "url": "/static/css/main.ee4e6a18.chunk.css"
+  },
+  {
+    "revision": "2b36fd88eb3984bc1ae8",
+    "url": "/static/js/2.e477b35b.chunk.js"
+  },
+  {
+    "revision": "503d4ebce8d05e16e016",
+    "url": "/static/js/main.b867fdb7.chunk.js"
+  },
+  {
+    "revision": "63e4bcf542d550101215",
+    "url": "/static/js/runtime~main.9a105661.js"
+  },
+  {
+    "revision": "e610de88957f1c41c21ddbb9513cd93d",
+    "url": "/static/media/Dosis.e610de88.woff"
+  }
+]);
